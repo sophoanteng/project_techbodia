f0bbe699c3b6204fcb6d33844dbfef2649a56d46
sokphorn <sokphorn080.xuanfang@gmail.com>
2022-10-26 10:49:12 +0700

    fetch data in to table by name and requirement

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div class="container">
+    <div slot="header">
+        <b>Test Project Techbodai</b>
+      </div>
+     <el-table
+         :data="tableData"
+         fit
+         highlight-current-row
+         style="margin-top: 2%; text-align: center;"
+         border
+      >
+       <el-table-column
+         prop="idd"
+         label="Idd"
+         width="60">
+         <template slot-scope="{ row }">
+          <span v-if="row.idd">{{ row.idd.root }}</span>
+        </template>
+       </el-table-column>
+       <el-table-column
+         prop="altSpellings"
+         label="altSpellings"
+         width="180">
+         <template slot-scope="{ row }">
+          <span v-if="row.altSpellings">{{ row.altSpellings[1] }}</span>
+        </template>
+       </el-table-column>
+       <el-table-column
+         prop="translations"
+         label="Native Name">
+         <template slot-scope="{ row }">
+          <span v-if="row.translations.zho">{{ row.translations.zho.official }}</span>
+        </template>
+       </el-table-column>
+       <el-table-column
+         prop="cca2"
+         label="Cca2"
+         width="100">
+         <template slot-scope="{ row }">
+          <span v-if="row.cca2">{{ row.cca2}}</span>
+        </template>
+       </el-table-column>
+       <el-table-column
+         prop="cca3"
+         label="Cca3"
+         width="100">
+         <template slot-scope="{ row }">
+          <span v-if="row.cca3">{{ row.cca3}}</span>
+        </template>
+       </el-table-column>
+       <el-table-column
+         prop="name"
+         label="Country">
+         <template slot-scope="{ row }">
+          <span v-if="row.name">{{ row.name.official}}</span>
+        </template>
+       </el-table-column>
+       <el-table-column
+         prop="flags"
+         label="Flags">
+         <template slot-scope="{ row }">
+          <el-image
+          v-if="row.flags"
+          style="width: 100px; height: 70px"
+          :src="row.flags.png"></el-image>
+        </template>
+       </el-table-column>
+     </el-table>
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        tableData: []
+      }
+    },
+    mounted(){
+      this.fetchData()
+    },
+    methods: {
+      fetchData() {
+        const getData = 'https://restcountries.com/v3.1/all'
+        this.$http.get(getData)
+        .then((res) => {
+          this.tableData = res.data
+          console.log(this.tableData)
+        })
+      }
+    }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+ .container{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  margin-top: 3%;
+ }
 </style>
